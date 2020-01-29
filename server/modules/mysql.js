84af5e42b3674a956c25fd9a01c3ef04486ec6c5
zoz0312@naver.com <zoz0312@naver.com>
2020-01-29 22:20:21 +0900

    module fix

--- server/modules/mysql.js
+++ server/modules/mysql.js
@@ -3,12 +3,12 @@ const server_config = require('/home/ahnhc/nomad_config.json');
 
 function database(){
 	this.connection = null;
-	this.open(){
+	this.open = () => {
 		this.connection = mysql.createConnection(server_config.server);
 	}
-	this.query(sql){
+	this.query = (sql) => {
 		return Promise((resolve, reject) => {
-			this.connection.query((sql, err, rows) =>{
+			this.connection.query(sql, (err, rows) =>{
 				if( err ){
 					return reject(err);
 				}
@@ -16,7 +16,7 @@ function database(){
 			});
 		});
 	}
-	this.close() {
+	this.close = () => {
 		return Promise((resolve, reject) => {
 			this.connection.end( err => {
 				if( err ){
@@ -30,4 +30,3 @@ function database(){
 }
 
 module.exports = database;
-
