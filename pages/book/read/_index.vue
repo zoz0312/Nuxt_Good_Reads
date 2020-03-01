a995a90d2dca09559cf6766ae37ebf37f8e9b921
zoz0312@naver.com <zoz0312@naver.com>
2020-03-01 16:00:32 +0900

    book read form css fix

--- pages/book/read/_index.vue
+++ pages/book/read/_index.vue
@@ -1,57 +1,51 @@
 <template>
-	<div>
-		<v-card
-			class="mx-auto"
-			max-width="800">
-			<v-card-text>
-				<v-row>
-					<v-col cols="11" class="display-1 text--primary">
-						{{ book_data.title }}
-					</v-col>
-					<v-col
-						cols="1"
-						v-if="$store.state.authUser !== null">
+	<div class="book-container">
+		<div class="b-image">
+			<v-card
+				max-width="240px">
+				<v-card-text>
+					<img class="book-img" :src="book_data.image" />
+				</v-card-text>
+				<v-card-action
+					v-if="$store.state.authUser !== null">
+					<div v-if="book_data.status === 1">
 						<v-icon
-							color="primary"
+							color="green darken-3"
 							@click="bookMark('del')"
-							v-if="book_data.status === 1"
 							x-large
 						>mdi-bookmark-check</v-icon>
+						<span class="green--text text--darken-3">책갈피 소장중!</span>
+					</div><div v-else>
 						<v-icon
 							color="primary"
 							@click="bookMark('add')"
-							v-else
 							x-large
 						>mdi-bookmark-plus</v-icon>
-					</v-col>
-				</v-row>
-			</v-card-text>
-			<v-card-text>
-				<v-row>
-					<v-col>
-						<img :src="book_data.image" />
-					</v-col>
-				</v-row>
-				<v-row>
-					<v-col cols="3">
-						Author
-					</v-col>
-					<v-col cols="9">
-						{{ book_data.author }}
-					</v-col>
-				</v-row>
-				<v-row>
-					<v-col cols="3">
-						Content
-					</v-col>
-					<v-col cols="9">
-						{{ book_data.contents }}
-					</v-col>
-				</v-row>
-			</v-card-text>
-			<v-card-actions>
-			</v-card-actions>
-		</v-card>
+						<span>책갈피 소장하기</span>
+					</div>
+				</v-card-action>
+			</v-card>
+		</div><div class="b-contain">
+			<div class="display-1">
+				{{ book_data.title }}
+			</div><div class="sub-title">
+				{{ book_data.author }}
+			</div>
+			<div>
+				<v-rating
+					class="comment-rate d-inline-block"
+					size="15"
+					color="red darken-1"
+					background-color="red lighten-4"
+					v-model="commentAvg.avgStar"
+					half-increments
+					readonly
+				></v-rating>
+				<span class="pr-2">{{ commentAvg.avgStar }}</span>
+				리뷰수 : <span>{{ commentAvg.commentCnt }}</span>개
+			</div>
+			{{ book_data.contents }}
+		</div>
 		<CommentList
 			v-bind:init="'write'"
 			v-bind:pIdx="pIdx"
@@ -64,6 +58,24 @@
 </template>
 
 <style scope>
+.book-container {
+	max-width: 1000px;
+	margin: auto;
+}
+.b-image {
+	display: inline-block;
+	margin-right: 2rem;
+}
+.book-img {
+	max-width: 100%;
+}
+.b-contain {
+	display: inline-block;
+	vertical-align: top;
+}
+.comment-rate > button {
+	padding: .1rem !important;
+}
 </style>
 
 <script>
@@ -95,8 +107,11 @@ export default {
 			}
 		}
 		const result = await axios.post(`${host}/book/detail/${pIdx}`, d);
+		const avgResult = await axios.post(`${host}/comment/info/${pIdx}`);
+		console.log('avgResult', avgResult);
 		return {
 			book_data: result.data.data,
+			commentAvg: avgResult.data.data,
 			pIdx
 		}
 	},
@@ -104,7 +119,11 @@ export default {
 		return {
 			book_data: null,
 			comment_data: null,
-			bookFlag: false
+			bookFlag: false,
+			commentAvg: {
+				commentCnt: 0,
+				avgStar: 0
+			}
 		}
 	},
 	methods: {
