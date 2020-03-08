6571d3ef7908290bcb07d52474fa250963c970c0
zoz0312@naver.com <zoz0312@naver.com>
2020-03-08 15:23:06 +0900

    main page add

---
+++ pages/book/list/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+		<BookList v-bind:bookinfo="book_list"/>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import axios from 'axios'
+import BookList from '~/components/BookList/BookList'
+
+export default {
+	data () {
+		return {
+			book_list: {}
+		}
+	},
+	async asyncData ({ params, req }) {
+		const host = req === undefined ? '' : 'http://127.0.0.1:3000';
+		const { data } = await axios.post(`${host}/book/all/1`);
+		return {
+			book_list: data.data
+		}
+	},
+	methods: {
+	},
+	mounted () {
+		/* this.getBookList(); */
+	},
+	components: {
+		BookList
+	}
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,32 +4,92 @@
     justify-center
     align-center
   >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-		<BookList v-bind:bookinfo="book_list"/>
-    </v-flex>
+		<div id="div-main">
+			<v-row class="text-row-m" align="center">
+				<v-col class="text-col">
+					<div class="main-text" transition="slide-y-transition">
+						Nuxt GoodReads
+					</div>
+					<div class="sub-text">
+						페이지 방문을 환영합니다.
+					</div>
+				</v-col>
+			</v-row>
+			<v-row class="text-row-s" align="center">
+				<v-col class="text-col">
+					<div class="main-text" transition="slide-y-transition">
+						회원이 되어 활동하여보세요.
+					</div>
+					<div class="sub-text">
+						Kakao와 Nuxt Goodreads로 로그인이 가능합니다.
+					</div>
+					<div class="sub-action">
+						로그인 / 회원가입
+					</div>
+				</v-col>
+			</v-row>
+			<v-row class="text-row-s last-div" align="center">
+				<v-col class="text-col">
+					<div class="main-text" transition="slide-y-transition">
+						책 등록부터 감상평까지
+					</div>
+					<div class="sub-text">
+						나만의 책을 추가하고 책 리스트를 관리하세요.
+					</div>
+				</v-col>
+			</v-row>
+		</div>
   </v-layout>
 </template>
 
-<script>
-import axios from 'axios'
-import BookList from '~/components/BookList/BookList'
+<style scope>
+#div-main {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	text-align: center;
+}
+.text-row-m,
+.text-row-s {
+	height: 100%;
+}
+.text-row-m {
+	background-color: #0F4C81;
+}
+.text-row-s {
+	background-color: #EEEEEE;
+	border-bottom: 1px solid #0F4C81;
+}
+.text-row-m > .text-col {
+	color: #EEEEEE;
+}
+.text-row-s > .text-col {
+	color: #0F4C81;
+}
+.text-row-s > .text-col > .sub-text {
+	color: #333333;
+}
+.text-row-s > .text-col > .sub-action {
+	margin-top: 2rem;
+}
+.main-text {
+	font-size: 5rem;
+}
+.sub-text {
+	font-size: 1rem;
+}
+.last-div {
+	margin-bottom: 36px;
+}
+</style>
 
+<script>
 export default {
 	data () {
 		return {
-			book_list: {}
 		}
 	},
 	async asyncData ({ params, req }) {
-		const host = req === undefined ? '' : 'http://127.0.0.1:3000';
-		const { data } = await axios.post(`${host}/book/all/1`);
-		return {
-			book_list: data.data
-		}
 	},
 	methods: {
 	},
@@ -37,7 +97,6 @@ export default {
 		/* this.getBookList(); */
 	},
 	components: {
-		BookList
 	}
 }
 </script>
